cd66822c1c4ab9c771ec52b2ea86d0005dc15b9c
kelrysro <kelryrodrigues86@gmail.com>
2023-06-02 16:47:40 -0300

    aula 02-06

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,12 @@
 <script setup>
-import { ref } from 'vue'
 import { livros } from '@/_data/livros.js'
+import{
+  carrinho,
+  removerItemCarrinho,
+  atualizaQuantidadeItem
+} from '@/_data/carrinho.js'
 
-const carrinho = ref({
-  itens: [],
-  total: 0
-})
-
-function atualizaQuantidadeItem(item) {
-  carrinho.value.total -= item.total
-  item.total = item.price * item.quantidade
-  carrinho.value.total += item.total
-}
-
-function removerItemCarrinho(item) {
-  const index = carrinho.value.itens.findIndex((i) => i.id === item.id)
-  carrinho.value.total -= item.total
-  carrinho.value.itens.splice(index, 1)
-}
-
-function adicionarAoCarrinho(livro) {
-  const index = carrinho.value.itens.findIndex((item) => item.id === livro.id)
-  if (index === -1) {
-    carrinho.value.itens.push({
-      ...livro,
-      quantidade: 1,
-      total: livro.price
-    })
-    carrinho.value.total += livro.price
-  } else {
-    carrinho.value.itens[index].quantidade++
-    carrinho.value.itens[index].total += livro.price
-    carrinho.value.total += livro.price
-  }
-}
+import CardLivro from './components/CardLivro.vue'
 
 function formatarPreco(preco) {
   return 'R$ ' + preco.toFixed(2).replace('.', ',')
@@ -44,19 +17,7 @@ function formatarPreco(preco) {
   <h1>Minha livraria</h1>
   <div class="container-geral">
     <div class="listagem-livros">
-      <div class="card-livro" v-for="livro in livros" :key="livro.id">
-        <div class="card-info-livro">
-          <div class="wrap-livro">
-            <img :src="livro.img" alt="Capa do livro" class="capa-livro" />
-          </div>
-          <p class="titulo-livro">{{ livro.title }}</p>
-          <p class="autor-livro">{{ livro.author }}</p>
-          <p class="preco-livro">{{ formatarPreco(livro.price) }}</p>
-        </div>
-        <div class="card-buttons-livros">
-          <button @click="adicionarAoCarrinho(livro)">Adicionar ao carrinho</button>
-        </div>
-      </div>
+      <card-livro v-for="livro in livros" :key="livro.id" :livro="livro"/>
     </div>
     <div class="carrinho">
       <h2>Meu carrinho</h2>
---
+++ src/components/CardLivro.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { adicionarAoCarrinho } from '@/_data/carrinho.js';
+const props =  defineProps({
+    livro: Object
+})
+
+function formatarPreco(preco) {
+  return 'R$ ' + preco.toFixed(2).replace('.', ',')
+}
+</script>
+
+<template>
+      <div class="card-livro" >
+        <div class="card-info-livro">
+          <div class="wrap-livro">
+            <img :src="props.livro.img" alt="Capa do livro" class="capa-livro" />
+          </div>
+          <p class="titulo-livro">{{ props.livro.title }}</p>
+          <p class="autor-livro">{{ props.livro.author }}</p>
+          <p class="preco-livro">{{ formatarPreco(props.livro.price) }}</p>
+        </div>
+        <div class="card-buttons-livros">
+          <button @click="adicionarAoCarrinho(props.livro)">Adicionar ao carrinho</button>
+        </div>
+      </div>
+    
+</template>
+
+<style scoped>
+.card-livro {
+  margin: 5px 10px;
+  padding: 10px;
+  background-color: beige;
+  border-radius: 10px;
+  width: 180px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.wrap-livro {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  border-radius: 10px;
+  width: 180px;
+  height: 270px;
+}
+.capa-livro {
+  width: 90%;
+  max-height: 100%;
+}
+
+.card-livro p {
+  margin: 0;
+}
+
+.card-livro .titulo-livro {
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+</style>
